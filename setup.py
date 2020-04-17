821fddfaa57884905ca4b1a6eb196211cbbb066e
iefthymio <Ilias.Efthymiopoulos@cern.ch>
2020-04-17 15:04:07 +0200

    created basic package structure files and folders

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='LHCTools',
+    version='1.0.1',
+    description='Scripts to use for LHC data analysis',
+    url='git@github.com:iefthymio/LHCTools.git',
+    author='Ilias Efthymiopoulos',
+    author_email='ilias.efthymiopoulos@cern.ch',
+    license='unlicense',
+    packages=['LHCTools'],
+    zip_safe=False
+)
